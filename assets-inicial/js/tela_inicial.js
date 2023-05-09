9acaab64e739fb335aba65dbbfa42db036d13cbb
Diegogete <ddrsilva@senacrs.com.br>
2023-05-09 12:04:11 -0300

    video

--- assets-inicial/js/tela_inicial.js
+++ assets-inicial/js/tela_inicial.js
@@ -1,4 +1,9 @@
 window.addEventListener('load', function(){
     $('.preloader').fadeOut(300);    
     
+    $('.btn-close').click(function(){
+   
+        $("iframe").attr("src", $("iframe").attr("src"));
+      })
+    
 })
